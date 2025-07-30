d194ff19c841d5505b446cd4b8c74309f493116d
Shahilcoder <shahilcoder@gmail.com>
2025-07-30 20:06:09 +0530

    integrate socket.io

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
+import fs from "fs";
+import https from "https";
 import express from "express";
+import { Server as socketio } from "socket.io";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.static("."));
 
-app.listen(PORT, (error) => {
+const key = fs.readFileSync("cert.key");
+const cert = fs.readFileSync("cert.crt");
+
+const httpsServer = https.createServer({key, cert}, app);
+
+const io = new socketio(httpsServer, {
+    cors: {
+        origin: [
+            "https://localhost"
+        ],
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", socket => {
+    console.log("Socket Connected");
+});
+
+httpsServer.listen(PORT, (error) => {
     if (error) {
         console.log(error);
         return;
